6fcd3cf584031b7dc0941f1c2c794bd56df14f70
weillium <will.liao@kellogg.northwestern.edu>
2022-02-22 17:11:02 +0000

    codealong

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,9 @@ class UsersController < ApplicationController
   end
 
   def create
+    plain_text_password = params["user"]["password"]
     @user = User.new(params["user"])
+    @user.password = BCrypt::Password.create(plain_text_password)
     @user.save
     redirect_to "/users/#{@user.id}"
   end
--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -6,3 +6,11 @@
 # 1. encrypt a secret and puts encrypted string
 # 2. decrypt the encrypted string
 # 3. check if decrypted value matches secret
+
+secret_password = "tacos"
+encrypted_password = BCrypt::Password.create(secret_password)
+puts encrypted_password
+
+password_to_test = "sushi"
+password_matches = BCrypt::Password.new(encrypted_password) == password_to_test
+puts password_matches
