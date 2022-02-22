327e3c52c6c97fd13a08b8063963b0cf3ca1073c
weillium <will.liao@kellogg.northwestern.edu>
2022-02-22 17:30:13 +0000

    lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     before_action :current_user
 
     def current_user 
-        if session["user"]
+        if session["user_id"]
             @current_user = User.find(session["user_id"])
         end
     end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.where({ user_id: @current_user.id })
     @task = Task.new
   end
 
   def create
     @task = Task.new(params["task"])
+    @task.user_id = @current_user.id
     @task.save
     redirect_to "/tasks"
   end
